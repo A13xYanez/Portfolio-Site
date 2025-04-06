808c4ecc685df7ed3283c9a253bc36cfee5f8474
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2025-04-06 15:50:13 -0700

    add a background gradient to landing

--- portfolio-site/src/components/landing/Landing.jsx
+++ portfolio-site/src/components/landing/Landing.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import './Landing.css';
 
 import { IoMenu } from "react-icons/io5";
 
+// TODO: use framer to make opacity effect for planet
+//TODO: Make menu open upwards when clicked
+//TODO: Make logo open socials when clicked
+//TODO: Make text effect when text is hovered
+//TODO: add purple backgrond gradient texture
 const Landing = () => {
     return (
         <main className='landing-container'>
@@ -11,7 +17,7 @@ const Landing = () => {
                     <IoMenu className='menu-icon' />
                 </div>
                 <div className="logo">
-                    <img src="/logo.png" alt="" />
+                    <img src="/H4.png" alt="" />
                 </div>
                 <div className="name">
                     <h2>Alex Yanez</h2>
@@ -20,8 +26,9 @@ const Landing = () => {
                     <h2>Software Engineer</h2>
                     <h2>Full-Stack Developer</h2>
                 </div>
-                    <spline-viewer url="https://prod.spline.design/n9t6fe6Uvtkn1ZE7/scene.splinecode"></spline-viewer>
-                    <div className="block"></div>
+                <div className="gradient"></div>
+                <spline-viewer url="https://prod.spline.design/n9t6fe6Uvtkn1ZE7/scene.splinecode"></spline-viewer>
+                <div className="block"></div>
             </div>
         </main>
     );
