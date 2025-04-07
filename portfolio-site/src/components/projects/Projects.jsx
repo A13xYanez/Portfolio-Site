1bd1f6f10a3aa0532eb0b56dca511b46083737a0
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2025-04-07 14:41:36 -0700

    update projects slider

--- portfolio-site/src/components/projects/Projects.jsx
+++ portfolio-site/src/components/projects/Projects.jsx
@@ -2,56 +2,110 @@ import React from 'react';
 import transition from '../global/transitions/Transition';
 import './Projects.css';
 
-import { VscTriangleRight } from "react-icons/vsc";
-import { VscTriangleLeft } from "react-icons/vsc";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 
 const Projects = () => {
     return (
-        <div className='projects-container'>
-            <div className="projects-wrapper">
-                <div className="projects-slider">
-                    <div className="project-item">
-                        <h1>asdfs</h1>
+        <div className="projects-container">
+            <Swiper
+                effect={'coverflow'}
+                grabCursor={false}
+                centeredSlides={true}
+                loop={true}
+                slidesPerView={'auto'}
+                coverflowEffect={{
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 2.5,
+                }}
+                pagination={{ el: '.swiper-pagination', clickable: true }}
+                navigation={{
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                    clickable: true,
+                }}
+                modules={[EffectCoverflow, Pagination, Navigation]}
+                className="swiper_container"
+            >
+                <SwiperSlide>
+                    <div className="project-content">
+                        <h2>Artisan Allure Fashions</h2>
+                        <div className="img-wrapper">
+
+                        </div>
+                        <div className="project-details">
+                            <h2>Website</h2>
+                        </div>
                     </div>
-                    <div className="project-item">
-                        <h1>asdfs</h1>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="project-content">
+                        <h2>Artisan  Fashions</h2>
+                        <div className="img-wrapper">
+
+                        </div>
+                        <div className="project-details">
+                            <h2>Website</h2>
+                        </div>
                     </div>
-                    <div className="project-item">
-                        <h1>asdfs</h1>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="project-content">
+                        <h2>Artisan Allure </h2>
+                        <div className="img-wrapper">
+
+                        </div>
+                        <div className="project-details">
+                            <h2>Website</h2>
+                        </div>
                     </div>
-                    <div className="project-item">
-                        <h1>asdfs</h1>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="project-content">
+                        <h2> Allure Fashions</h2>
+                        <div className="img-wrapper">
+
+                        </div>
+                        <div className="project-details">
+                            <h2>Website</h2>
+                        </div>
                     </div>
-                    <div className="project-item">
-                        <h1>asdfs</h1>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="project-content">
+                        <h2>  Fashions</h2>
+                        <div className="img-wrapper">
+
+                        </div>
+                        <div className="project-details">
+                            <h2>Website</h2>
+                        </div>
                     </div>
-                    <div className="project-item">
-                        <h1>asdfs</h1>
+                </SwiperSlide>
+
+                <div className="slider-controler">
+                    <div className="swiper-button-prev slider-arrow">
+
                     </div>
-                    <div className="project-item">
-                        <h1>asdfs</h1>
+                    <div className="swiper-button-next slider-arrow">
+
                     </div>
-                    <div className="project-item">
-                        <h1>asdfs</h1>
-                    </div>
-                    <div className="project-item">
-                        <h1>asdfs</h1>
-                    </div>
-                    <div className="project-item">
-                        <h1>asdfs</h1>
-                    </div>
+                    <div className="swiper-pagination"></div>
                 </div>
-            </div>
-            <div className="project-slider-controlls">
-                <div className="projects-controll-btn">
-                    <VscTriangleLeft className='controll-btn-icon' />
-                </div>
-                <div className="projects-controll-btn">
-                    <VscTriangleRight className='controll-btn-icon' />
-                </div>
-            </div>
+            </Swiper>
         </div>
     );
-};
+}
 
 export default transition(Projects);
