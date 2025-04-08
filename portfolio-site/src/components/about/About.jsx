fa9985e3bc532881ae4241fe83f813691a8abe2b
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2025-04-08 12:05:06 -0700

    discard bento grid design and add folder images

--- portfolio-site/src/components/about/About.jsx
+++ portfolio-site/src/components/about/About.jsx
@@ -1,190 +1,89 @@
 import React from 'react';
-import { useEffect, useState, useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
 import transition from '../global/transitions/Transition'
 import './About.css'
-import { FaLinkedin } from "react-icons/fa6";
-import { FaGithub } from "react-icons/fa";
-import { FaAngleLeft } from "react-icons/fa";
 
-import { motion } from 'framer-motion';
+import fold from '../../assets/Folder.svg';
 
 const About = () => {
-    const about = useRef(null);
-    const work = useRef(null);
-    const education = useRef(null);
-    const skills = useRef(null);
+    const container = useRef(null);
+    const folder = useRef(null);
+    const isClicked = useRef(false);
+    const coords = useRef({
+        startX: 330,
+        startY: 340,
+        lastX: 330,
+        lastY: 340
+    });
 
-    // Container Lighting Effect
     useEffect(() => {
-        // Target Containers Referenced
-        const aboutTarget = about.current;
-        aboutTarget.onmousemove = e => handleOnMouseMove(e);
-        const workTarget = work.current;
-        workTarget.onmousemove = e => handleOnMouseMove(e);
-        const educationTarget = education.current;
-        educationTarget.onmousemove = e => handleOnMouseMove(e);
-        const skillsTarget = skills.current;
-        skillsTarget.onmousemove = e => handleOnMouseMove(e);
+        // reference the elements to drag and its container 
+        if (!container.current || !folder.current) return;
+        const folderTarget = folder.current;
+        const containerTarget = container.current;
 
-        // Tracking Function
-        const handleOnMouseMove = e => {
-            const { currentTarget: target } = e;
+        // event listener function
+        const onMouseDown = (e) => {
+            isClicked.current = true;
+            folderTarget.style.cursor = "grab";
+            coords.current.startX = e.clientX;
+            coords.current.startY = e.clientY;
+        };
 
-            const rect = target.getBoundingClientRect(),
-                x = e.clientX - rect.left,
-                y = e.clientY - rect.top;
+        const onMouseUp = (e) => {
+            isClicked.current = false;
+            folderTarget.style.cursor = "pointer";
+            coords.current.lastX = folderTarget.offsetLeft;
+            coords.current.lastY = folderTarget.offsetTop;
+        };
 
-            target.style.setProperty("--mouse-x", `${x}px`);
-            target.style.setProperty("--mouse-y", `${y}px`);
+        const onMouseMove = (e) => {
+            if (!isClicked.current) return;
+
+            const nextX = e.clientX - coords.current.startX + coords.current.lastX;
+            const nextY = e.clientY - coords.current.startY + coords.current.lastY;
+
+            folderTarget.style.cursor = "grabbing";
+            folderTarget.style.top = `${nextY}px`;
+            folderTarget.style.left = `${nextX }px`;
         }
-    }, []);
 
-    const [isDisplayed, setIsDisplayed] = useState(true);
+        // attatch event listeners
+        folderTarget.addEventListener('mousedown', onMouseDown);
+        folderTarget.addEventListener('mouseup', onMouseUp);
+        containerTarget.addEventListener('mousemove', onMouseMove);
+        containerTarget.addEventListener('mouseleave', onMouseUp);
+
+        // remove event listeners
+        const cleanUp = () => {
+            folderTarget.removeEventListener('mousedown', onMouseDown);
+            folderTarget.removeEventListener('mouseup', onMouseUp);
+            containerTarget.removeEventListener('mousemove', onMouseMove);
+            containerTarget.removeEventListener('mouseleave', onMouseUp);
+        };
+
+        return cleanUp;
+    }, []);
     return (
-        <div className="contain-about">
-            <div className="work-experience" ref={work}>
-                <h2>Experience</h2>
-                <ul className="work-experience--list">
-                    <Link to='https://unifoodie.xyz/'>
-                        <li className='work-experience--list-item'>
-                            <img src="" alt="unifoodi company logo" /> 
-                            <div className='group-work-info'>
-                                <div className="company-info">
-                                    <p>UNiFOODi</p>
-                                    <p className='info-seperator'>|</p>
-                                    <p style={{color: "#929292"}}>Full-Stack Developer</p>
-                                </div>
-                                <div className="working-dates">
-                                    <div className='blinking-light blue-light'>
-                                        <div />
-                                    </div>
-                                    <p style={{color: "#929292"}}>Feb 2025 - Present</p>
-                                </div>
-                            </div>
-                        </li>
-                    </Link>
-                    <Link to='https://vista.ucmerced.edu/'>
-                        <li className='work-experience--list-item'>
-                            <img src="" alt="unifoodi company logo" /> 
-                            <div className='group-work-info'>
-                                <div className="company-info">
-                                    <p>VISTA</p>
-                                    <p className='info-seperator'>|</p>
-                                    <p style={{color: "#929292"}}>Web Developer</p>
-                                </div>
-                                <div className="working-dates">
-                                    <div className='blinking-light blue-light'>
-                                        <div />
-                                    </div>
-                                    <p style={{color: "#929292"}}>Jan 2025 - Present</p>
-                                </div>
-                            </div>
-                        </li>
-                    </Link>
-                    <Link to='https://www.bayvalleytech.com/'>
-                        <li className='work-experience--list-item'>
-                            <img src="" alt="unifoodi company logo" /> 
-                            <div className='group-work-info'>
-                                <div className="company-info">
-                                    <p>Bay Valley Tech</p>
-                                    <p className='info-seperator'>|</p>
-                                    <p style={{color: "#929292"}}>Full-Stack Developer</p>
-                                </div>
-                                <div className="working-dates">
-                                    <div className='blinking-light green-light'>
-                                        <div />
-                                    </div>
-                                    <p style={{color: "#929292"}}>Mar 2024 - Aug 2024</p>
-                                </div>
-                            </div>
-                        </li>
-                    </Link>
-                </ul>
+        <div ref={container} className="about-container">
+            <div className="folder-container about-me-folder" ref={folder}>
+                <img src={fold} draggable="false" alt='Folder Icon' className="folder-img" />
+                <p>About Me</p>
             </div>
-            <div className="about" ref={about}>
-                <h3>Hey There, I'm Alex...</h3>
-                <p className='about-roles'>Full-Stack Developer & Software Engineer.</p>
-                <p className='about-details'>
-                    I create beautiful web and mobile apps with a lot of attention to detail.
-                </p>
-                <div className="about-socials">
-                    <Link to='https://www.linkedin.com/in/a13x-yanez/'><FaLinkedin className='about-social-icon' /></Link>
-                    <Link to='https://github.com/A13xYanez'><FaGithub className='about-social-icon' /></Link>
-                </div>
-                <div className="about-name">
-                    <div className="first-name">
-                        <p className='first-layer'>Alex</p>
-                        <p className='second-layer'>Alex</p>
-                        <p className='third-layer'>Alex</p>
-                    </div>
-                    <div className="last-name">
-                        <p className='first-layer'>Yanez</p>
-                        <p className='second-layer'>Yanez</p>
-                        <p className='third-layer'>Yanez</p>
-                    </div>
-                </div>
-                <p className='about-details'>
-                    From making my first program on Code.org when I was 11 y/o to working on exciting and challenging projects,
-                    I have come a long way and can't wait for what the future holds for me.
-                </p>
+            <div className="folder-container work-experience-folder">
+                <img src={fold} draggable="false" alt='Folder Icon' className="folder-img" />
+                <p>Experience</p>
             </div>
-            <div className="education" ref={education}>
-                <motion.div 
-                initial={{opacity: 1}} 
-                animate={!isDisplayed ? {opacity: 0, display: "none", transition: {duration: 1}} : {opacity: 1, display: "block", transition: {duration: 1, delay: 1}}}
-                className='display-university'
-                >
-                    <Link to='https://www.ucmerced.edu/'>
-                        <div className="university-img">
-                            <img src="" alt="" />
-                        </div>
-                    </Link>
-                    <p className='university-info'>Degree :</p>
-                    <p className='university-info'>Computer Science & Engineering</p>
-                    <button className='btn-certs' onClick={() => setIsDisplayed(false)}>View Courses</button>
-                </motion.div>
-                <motion.div 
-                initial={{opacity: 0 , display: "none"}} 
-                animate={!isDisplayed ? {opacity: 1, display: "block", transition: {duration: 1, delay: 1}} : {opacity: 0, display: "none", transition: {duration: 1}}} 
-                className="display-courses">
-                    <div className="courses-exit" onClick={() => setIsDisplayed(true)}>
-                        <FaAngleLeft className='courses-back-arrow' />
-                        <p>Back</p>
-                    </div>
-                    <ul className="course-list">
-                        <p>Completed:</p>
-                        <li>Intro to Programming</li>
-                        <li>Advanced Programming</li>
-                        <li>Data Structures</li>
-                        <li>Computer Organization and Assembely Language</li>
-                        <p>Planned:</p>
-                        <li>Algorithm Design and Analysis</li>
-                        <li>Software Engineering</li>
-                    </ul>
-                </motion.div>
+            <div className="folder-container education-folder">
+                <img src={fold} draggable="false" alt='Folder Icon' className="folder-img" />
+                <p>Education</p>
             </div>
-            <div className="skills" ref={skills}>
-                <h2>Skills</h2>
-                <div className="skill-items">
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/fastapi/fastapi-original.svg" />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flutter/flutter-original.svg" />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dart/dart-original.svg" />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg" />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
-                </div>
-                <p>
-                    Visit my projects section to see the full list of skills
-                    and the work done with these skills.
-                </p>
+            <div className="folder-container skills-folder">
+                <img src={fold} draggable="false" alt='Folder Icon' className="folder-img" />
+                <p>Skills</p>
             </div>
         </div>
-    );
+    )
 };
 
 export default transition(About);
