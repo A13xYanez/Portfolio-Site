4f9e2fc4ee447da46c4b22a8b19e0cf531200e8d
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2025-04-07 19:14:32 -0700

    add socials to navbar

--- portfolio-site/src/components/global/nav/Nav.jsx
+++ portfolio-site/src/components/global/nav/Nav.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { easeInOut, motion } from 'framer-motion';
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 import './Nav.css';
 
 const Nav = () => {
+    const [openNav, setOpenNav] = useState(false);
+
     return (
         <nav>
-            <div className="logo">
-                <img src="/logo.svg" alt="Website Logo" />
-            </div>
+            <motion.div 
+            initial={false}
+            animate={{top: openNav ? 0 : -90, transition: {ease: easeInOut}}}
+            className="contain-nav-controlls">
+                <img src='/logo.svg' className='nav-logo' onClick={() => setOpenNav(!openNav)} />
+                <div>
+                    <div>
+                        <Link to='https://www.linkedin.com/in/a13x-yanez/'><FaLinkedin className='nav-link-icon' /></Link>
+                    </div>
+                    <div>
+                        <Link to='https://github.com/A13xYanez'><FaGithub className='nav-link-icon'  /></Link>
+                    </div>
+                </div>
+            </motion.div>
             <div className="name">
                 <h2>Alex Yanez</h2>
             </div>
