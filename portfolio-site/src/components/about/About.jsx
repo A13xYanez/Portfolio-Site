65122b1cbf2327655c51587aa4286b661879b4cc
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2025-04-09 19:13:41 -0700

    create education window with degree tab

--- portfolio-site/src/components/about/About.jsx
+++ portfolio-site/src/components/about/About.jsx
@@ -11,10 +11,11 @@ import bayvalleytech from '../../assets/bay_valley_tech_logo.jpg';
 import vista from '../../assets/valley_institute_for_sustainability_technology_agriculture_logo.jpg';
 import unifoodi from '../../assets/unifoodi_logo.png';
 import { VscTriangleDown } from "react-icons/vsc";
+import ucmlogo from '../../assets/UCMLogo.png';
 
 //TODO:
 //Change the p tags to textarea so that the windows become resizeable.
-//Set a minimun width and height so that they cannot be too small.
+//Set a minimun and max width and height so that they cannot be too small or big.
 
 const About = () => {
     const container = useRef(null);
@@ -59,6 +60,10 @@ const About = () => {
             folderTarget.style.left = `${nextX }px`;
         }
 
+        const onDoubleClick = () => {
+            setAboutIsOpen(true);
+        };
+
         // attatch event listeners
         folderTarget.addEventListener('mousedown', onMouseDown);
         folderTarget.addEventListener('mouseup', onMouseUp);
@@ -71,6 +76,7 @@ const About = () => {
             folderTarget.removeEventListener('mouseup', onMouseUp);
             containerTarget.removeEventListener('mousemove', onMouseMove);
             containerTarget.removeEventListener('mouseleave', onMouseUp);
+            folderTarget.addEventListener("dblclick", onDoubleClick);
         };
 
         return cleanUp;
@@ -79,6 +85,11 @@ const About = () => {
     const [infoIsOpen, setInfoIsOpen] = useState(true);
     const [aboutIsOpen, setAboutIsOpen] = useState(false);
     const [experienceIsOpen, setExperienceIsOpen] = useState(false);
+    const [educationIsOpen, setEducationIsOpen] = useState(false);
+
+    const [isDegreeTabOpen, setIsDegreeTabOpen] = useState(true);
+    const [isCoursesTabOpen, setIsCoursesTabOpen] = useState(false);
+    const [isClubsTabOpen, setIsClubsTabOpen] = useState(false);
 
     const [selected, setSelected] = useState(null);
     const toggle = (i) => {
@@ -91,7 +102,7 @@ const About = () => {
 
     return (
         <div ref={container} className="about-container">
-            <div className="folder-container about-me-folder" ref={folder} onClick={() => setAboutIsOpen(true)}>
+            <div className="folder-container about-me-folder" ref={folder}>
                 <img src={folderIcon} draggable="false" alt='Folder Icon' className="folder-img" />
                 <p>About Me</p>
             </div>
@@ -99,7 +110,7 @@ const About = () => {
                 <img src={folderIcon} draggable="false" alt='Folder Icon' className="folder-img" />
                 <p>Experience</p>
             </div>
-            <div className="folder-container education-folder">
+            <div className="folder-container education-folder" onClick={() => setEducationIsOpen(true)}>
                 <img src={folderIcon} draggable="false" alt='Folder Icon' className="folder-img" />
                 <p>Education</p>
             </div>
@@ -123,7 +134,7 @@ const About = () => {
                             <img src={msgInfo} draggable="false" alt="Information Icon" />
                             <h2>Read me:</h2>
                         </div>
-                        <p>To access the information about me, click or tap on the folders.</p>
+                        <p>To access the information about me, <strong>double click</strong> on the folders.</p>
                         <br/>
                         <p>Please note, you can move all the items on this page by dragging them with your mouse.</p>
                         <p>This feature is disabled on mobile devices for a better user experience.</p>
@@ -184,6 +195,65 @@ const About = () => {
                                 </div>
                             </div>
                         ))}
+                </div>
+            }
+
+            {educationIsOpen &&
+                <div className="education-popup-window-container">
+                    <div className="window-title-bar">
+                        <div className="title-bar-left">
+                            <h2>Education</h2>
+                        </div>
+                        <div className="title-bar-right">
+                            <img src={closeIcon} alt="Close Button Icon" draggable="false" onClick={() => setEducationIsOpen(false)} />
+                        </div>
+                    </div>
+                    <div className="education-content-wrapper">
+                        <div className="education-section-tabs">
+                            <div className="education-tab" onClick={() => {setIsDegreeTabOpen(true); setIsCoursesTabOpen(false); setIsClubsTabOpen(false);}}>
+                                <p>Degree</p>
+                            </div>
+                            <div className="education-tab"  onClick={() => {setIsDegreeTabOpen(false); setIsCoursesTabOpen(true); setIsClubsTabOpen(false);}}>
+                                <p>Courses</p>
+                            </div>
+                            <div className="education-tab" onClick={() => {setIsDegreeTabOpen(false); setIsCoursesTabOpen(false); setIsClubsTabOpen(true);}}>
+                                <p>Clubs</p>
+                            </div>
+                        </div>
+                        <div className="education-window-content">
+                            {isDegreeTabOpen &&
+                                <div className="education-degree-tab">
+                                    <div className="university-logo">
+                                        <img src={ucmlogo} alt="" />
+                                    </div>
+                                    <div className="univeristy-details">
+                                        <p className='university-details-section-title'>University:</p>
+                                        <p>The University of California, Merced</p>
+                                        <br/>
+                                        <br/>
+                                        <p className='university-details-section-title'>Degree:</p>
+                                        <p>Computer Science & Engineering</p>
+                                        <br />
+                                        <br />
+                                        <p className='university-details-section-title'>Graduation Date:</p>
+                                        <p>Expected 2026</p>
+                                    </div>
+                                </div>
+                            }
+
+                            {isCoursesTabOpen &&
+                                <div className="education-courses-tab">
+
+                                </div>
+                            }
+
+                            {isClubsTabOpen &&
+                                <div className="education-clubs-tab">
+
+                                </div>
+                            }
+                        </div>
+                    </div>
                 </div>
             }
         </div>
