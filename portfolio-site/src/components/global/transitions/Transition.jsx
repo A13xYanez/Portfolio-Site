c4df73a4c4e2337328013f27410b0bd2e462ec81
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2025-04-20 17:35:15 -0700

    create new page transition

--- portfolio-site/src/components/global/transitions/Transition.jsx
+++ portfolio-site/src/components/global/transitions/Transition.jsx
@@ -2,6 +2,90 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import './Transition.css';
 
+// Sliding Transition
+const transition = (Component) => {
+    const animation = (variants, custom) => {
+        return {
+            initial: "initial",
+            animate: "enter",
+            exit: "exit",
+            variants,
+            custom
+        };
+    };
+
+    const slideIn = {
+        initial: {
+            scaleY: 0
+        },
+        enter: (i) => ({
+            scaleY: 0,
+            transition: {
+                duration: 1,
+                ease: [0.22, 1, 0.36, 1]
+            }
+        }),
+        exit: {
+            scaleY: 1,
+            transition: {
+                duration: 1,
+                ease: [0.22, 1, 0.36, 1]
+            }
+        }
+    };
+
+    const slideOut = {
+        initial: {
+            scaleY: 1
+        },
+        enter: (i) => ({
+            scaleY: 0,
+            transition: {
+                duration: 1,
+                ease: [0.22, 1, 0.36, 1]
+            }
+        }),
+        exit: {
+            scaleY: 0,
+            transition: {
+                duration: 1,
+                ease: [0.22, 1, 0.36, 1]
+            }
+        }
+    };
+
+    const overlay = {
+        initial: {
+            opacity: 1,
+        },
+        enter: {
+            opacity: 0,
+        },
+        exit: {
+            opacity: 1,
+        }
+    };
+
+    return () => (
+        <>
+            <motion.div {...animation(overlay)} className='transition-background' />
+            <motion.div
+                className="slide-in"
+                {...animation(slideIn)}
+            />
+            <motion.div
+                className="slide-out"
+                {...animation(slideOut)}
+            />
+            <Component />
+        </>
+    )
+};
+
+
+
+// Stairs Transiion (Uncomment when needed)
+/*
 const transition = (Component) => {
     const animation = (variants, custom) => {
         return {
@@ -64,5 +148,6 @@ const transition = (Component) => {
         </div>
     )
 }
+*/
 
 export default transition;
--- portfolio-site/src/main.jsx
+++ portfolio-site/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import '@splinetool/viewer';
 import './index.css'
 import App from './App.jsx'
 
