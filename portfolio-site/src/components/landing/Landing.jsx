a52bed66bc78fbb72ab2f9413ca7597b27c832a8
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2025-05-04 14:56:02 -0700

    stall 3d object load while preloader plays

--- portfolio-site/src/components/landing/Landing.jsx
+++ portfolio-site/src/components/landing/Landing.jsx
@@ -1,10 +1,16 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState, useEffect } from 'react';
 import transition from '../global/transitions/Transition';
 const Spline = React.lazy(() => import('@splinetool/react-spline'));
 import './Landing.css';
 
-
 const Landing = () => {
+    const [ready, setReady] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setReady(true), 1000); 
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <main className='landing-container'>
             <div className="roles">
@@ -13,10 +19,12 @@ const Landing = () => {
             </div>
             <div className="gradient" />
             <Suspense>
-                <Spline
-                    className='planet'
-                    scene="https://prod.spline.design/Zrxlwv5n5Gc0cpJy/scene.splinecode" 
-                />
+                {ready &&
+                    <Spline
+                        className='planet'
+                        scene="https://prod.spline.design/Zrxlwv5n5Gc0cpJy/scene.splinecode" 
+                    />
+                }
             </Suspense>
             <div className="block" />
         </main>
